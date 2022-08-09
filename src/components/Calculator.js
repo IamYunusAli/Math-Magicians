847551be02cde233f095c69df390df7f728629a4
iamyunusali <aliyunus291@gmail.com>
2022-08-09 15:54:10 +0300

    Add event on buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,38 +1,57 @@
 import React, { PureComponent } from 'react';
 import './Calculator.css';
+import calculate from '../logic/calculate';
 
 class Calculator extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    const num = e.target.innerText;
+    this.setState((state) => calculate(state, num));
+  }
+
   render() {
+    const { next, total } = this.state;
+    const displayHandler = (e) => {
+      const val = e.target.textContent;
+      this.setState((state) => ({ ...state, total: val }));
+    };
     return (
       <div className="calculator">
-        <div className="output" />
-        <div className="row">
-          <button type="button">AC</button>
-          <button type="button">+/-</button>
-          <button type="button">%</button>
-          <button className="sign" type="button">÷</button>
+        <div className="output" onChange={displayHandler}>
+          {next || total || 0}
         </div>
         <div className="row">
-          <button type="button">7</button>
-          <button type="button">8</button>
-          <button type="button">9</button>
-          <button className="sign" type="button">x</button>
+          <button type="button" onClick={this.handleClick}>AC</button>
+          <button type="button" onClick={this.handleClick}>+/-</button>
+          <button type="button" onClick={this.handleClick}>%</button>
+          <button className="sign" type="button" onClick={this.handleClick}>÷</button>
         </div>
         <div className="row">
-          <button type="button">4</button>
-          <button type="button">5</button>
-          <button type="button">6</button>
-          <button className="sign" type="button">-</button>
+          <button type="button" onClick={this.handleClick}>7</button>
+          <button type="button" onClick={this.handleClick}>8</button>
+          <button type="button" onClick={this.handleClick}>9</button>
+          <button className="sign" type="button" onClick={this.handleClick}>x</button>
         </div>
         <div className="row">
-          <button type="button">1</button>
-          <button type="button">2</button>
-          <button type="button">3</button>
-          <button className="sign" type="button">+</button>
+          <button type="button" onClick={this.handleClick}>4</button>
+          <button type="button" onClick={this.handleClick}>5</button>
+          <button type="button" onClick={this.handleClick}>6</button>
+          <button className="sign" type="button" onClick={this.handleClick}>-</button>
         </div>
         <div className="row">
-          <button type="button">0</button>
-          <button className="equal sign" type="button">=</button>
+          <button type="button" onClick={this.handleClick}>1</button>
+          <button type="button" onClick={this.handleClick}>2</button>
+          <button type="button" onClick={this.handleClick}>3</button>
+          <button className="sign" type="button" onClick={this.handleClick}>+</button>
+        </div>
+        <div className="row">
+          <button type="button" onClick={this.handleClick}>0</button>
+          <button className="equal sign" type="button" onClick={this.handleClick}>=</button>
         </div>
       </div>
     );
