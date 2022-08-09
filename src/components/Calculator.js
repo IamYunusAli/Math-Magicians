1f85b4ea62e716200348159c074a3f270775bc98
iamyunusali <aliyunus291@gmail.com>
2022-08-09 20:17:10 +0300

    refactor with hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,61 +1,56 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import './Calculator.css';
 import calculate from '../logic/calculate';
 
-class Calculator extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.handleClick = this.handleClick.bind(this);
-  }
+function Calculator() {
+  const [state, setState] = useState(0);
 
-  handleClick(e) {
+  const handleClick = (e) => {
     const num = e.target.innerText;
-    this.setState((state) => calculate(state, num));
-  }
-
-  render() {
-    const { next, total } = this.state;
-    const displayHandler = (e) => {
-      const val = e.target.textContent;
-      this.setState((state) => ({ ...state, total: val }));
-    };
-    return (
-      <div className="calculator">
-        <div className="output" onChange={displayHandler}>
-          {next || total || 0}
-        </div>
-        <div className="row">
-          <button type="button" onClick={this.handleClick}>AC</button>
-          <button type="button" onClick={this.handleClick}>+/-</button>
-          <button type="button" onClick={this.handleClick}>%</button>
-          <button className="sign" type="button" onClick={this.handleClick}>÷</button>
-        </div>
-        <div className="row">
-          <button type="button" onClick={this.handleClick}>7</button>
-          <button type="button" onClick={this.handleClick}>8</button>
-          <button type="button" onClick={this.handleClick}>9</button>
-          <button className="sign" type="button" onClick={this.handleClick}>x</button>
-        </div>
-        <div className="row">
-          <button type="button" onClick={this.handleClick}>4</button>
-          <button type="button" onClick={this.handleClick}>5</button>
-          <button type="button" onClick={this.handleClick}>6</button>
-          <button className="sign" type="button" onClick={this.handleClick}>-</button>
-        </div>
-        <div className="row">
-          <button type="button" onClick={this.handleClick}>1</button>
-          <button type="button" onClick={this.handleClick}>2</button>
-          <button type="button" onClick={this.handleClick}>3</button>
-          <button className="sign" type="button" onClick={this.handleClick}>+</button>
-        </div>
-        <div className="row">
-          <button type="button" onClick={this.handleClick}>0</button>
-          <button className="equal sign" type="button" onClick={this.handleClick}>=</button>
-        </div>
+    setState((state) => calculate(state, num));
+  };
+  const { next, total } = state;
+  const displayHandler = (e) => {
+    const val = e.target.textContent;
+    setState((state) => ({ ...state, total: val }));
+  };
+  return (
+    <div className="calculator">
+      <div className="output" onChange={displayHandler}>
+        {next}
+        {total}
+        {0}
       </div>
-    );
-  }
+      <div className="row">
+        <button type="button" onClick={handleClick}>AC</button>
+        <button type="button" onClick={handleClick}>+/-</button>
+        <button type="button" onClick={handleClick}>%</button>
+        <button className="sign" type="button" onClick={handleClick}>÷</button>
+      </div>
+      <div className="row">
+        <button type="button" onClick={handleClick}>7</button>
+        <button type="button" onClick={handleClick}>8</button>
+        <button type="button" onClick={handleClick}>9</button>
+        <button className="sign" type="button" onClick={handleClick}>x</button>
+      </div>
+      <div className="row">
+        <button type="button" onClick={handleClick}>4</button>
+        <button type="button" onClick={handleClick}>5</button>
+        <button type="button" onClick={handleClick}>6</button>
+        <button className="sign" type="button" onClick={handleClick}>-</button>
+      </div>
+      <div className="row">
+        <button type="button" onClick={handleClick}>1</button>
+        <button type="button" onClick={handleClick}>2</button>
+        <button type="button" onClick={handleClick}>3</button>
+        <button className="sign" type="button" onClick={handleClick}>+</button>
+      </div>
+      <div className="row">
+        <button type="button" onClick={handleClick}>0</button>
+        <button className="equal sign" type="button" onClick={handleClick}>=</button>
+      </div>
+    </div>
+  );
 }
 
 export default Calculator;
