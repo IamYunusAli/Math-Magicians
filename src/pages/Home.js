67db6b00337a241bbe22b6e4ed27b6acc4890cdf
iamyunusali <aliyunus291@gmail.com>
2022-08-15 17:21:43 +0300

    Add pages

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './pages.module.css';
+
+const Home = () => (
+  <div className={styles.home}>
+    <h2>welcome to homepage</h2>
+    <p>
+      Mathemagician is a neologism, specifically a portmanteau,
+      that combines mathematician and magician. A great number of
+      self-working mentalism tricks rely on mathematical principles.
+      Max Maven often utilizes this type of magic in his performance.
+    </p>
+  </div>
+);
+
+export default Home;
