921f2aba7bf29a1150ca78223dfc17cd2ad8128c
iamyunusali <aliyunus291@gmail.com>
2022-08-17 13:04:23 +0300

    fix linter errors

--- src/test/Logictest/calculate.test.js
+++ src/test/Logictest/calculate.test.js
@@ -1,24 +1,26 @@
-import calculate from "../../logic/calculate";
+import calculate from '../../logic/calculate';
 
 describe('Unit testing of the calculate', () => {
-    test('AC', () => {
-        const result = calculate({}, 'AC');
-        expect(result).toEqual({
-            total: null,
-            next: null,
-            operation: null,
-    })
-    })
+  test('AC', () => {
+    const result = calculate({}, 'AC');
+    expect(result).toEqual({
+      total: null,
+      next: null,
+      operation: null,
+    });
+  });
 
-    test('equal', () => {
-        const result = calculate({ 
-            total: 8,
-            next: 2,
-            operation: '÷',}, '=');
+  test('equal', () => {
+    const result = calculate({
+      total: 8,
+      next: 2,
+      operation: '÷',
+    }, '=');
 
-        expect(result).toEqual({ total: '4',
-            next: null,
-            operation: null,})
-        
-    })
-})
+    expect(result).toEqual({
+      total: '4',
+      next: null,
+      operation: null,
+    });
+  });
+});
